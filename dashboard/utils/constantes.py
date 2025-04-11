8793dafe8efe6d2cd1ec7c34884b10741d2fa9c4
Moussa Sow Dieme <lermitejiraya05@gmail.com>
2025-04-11 06:35:51 +0000

    add function classify_ndvi and calcul_class_stats

--- dashboard/utils/constantes.py
+++ dashboard/utils/constantes.py
@@ -1,4 +1,4 @@
-DATA_DIR = "data"  
+DATA_DIR = "data"  # A modifier si stockage dans AWS 
 NDVI_MIN = -1
 NDVI_MAX = 1
 NDVI_CLASSES = {
@@ -7,3 +7,4 @@ NDVI_CLASSES = {
     "Végétation moyenne": [0.4, 0.6],
     "Végétation dense": [0.6, 1]
 }
+pixel_surface_m2=10*10 # pour le calcul des stats des classes de NDVI
--- dashboard/utils/data_loader.py
+++ dashboard/utils/data_loader.py
@@ -1,6 +1,14 @@
+# -----------------------------------------------------------------------------
+#
+# Ce script postule que nous continuons avec la notation du style  Foret_Classee_de_Richard-Toll_01-01-01-02-2019.tif
+# et qu'on télécharge toujours des images composites entre le 01 janvier et le 01 fevrier
+# -----------------------------------------------------------------------------
+
 import glob
 import os
 from constantes import *
+import rasterio
+import numpy as np
 
 
 def get_forest_names():
@@ -35,3 +43,69 @@ def get_available_years():
             continue
     return sorted(list(years))
 
+
+def get_file_path(forest_name, year):
+    """
+    trouver le fichier correspondant à une forêt et une année
+    """
+    pattern = os.path.join(DATA_DIR, f"Foret_Classee_de_{forest_name}_01-01-01-02-{year}.tif")
+    matching_files = glob.glob(pattern)
+    if matching_files:
+        return matching_files[0]
+    return None
+
+def calcul_ndvi(image_path):
+    with rasterio.open(image_path) as src:
+        red_band = src.read(3) 
+        nir_band = src.read(4) 
+        ndvi = (nir_band.astype(float) - red_band.astype(float)) / (nir_band.astype(float) + red_band.astype(float))
+        
+        return ndvi
+
+def classify_ndvi(ndvi_matrix):
+    # init d'un zero_array de meme dimension que ndvi_array
+    ndvi_class_map = np.zeros_like(ndvi_matrix)
+    category_counter = 1  # indice 1 pour la premiére classe 
+
+    for category_label, (lower_bound, upper_bound) in NDVI_CLASSES.items():
+        category_mask = (ndvi_matrix >= lower_bound) & (ndvi_matrix <= upper_bound) # Un tableau booléen
+        ndvi_class_map[category_mask] = category_counter # remplace les 0 par le numero de la classe NDVI
+        category_counter += 1  
+    return ndvi_class_map 
+
+def calcul_class_stats(ndvi_class_map):
+    """
+    Calcule les statistiques des classes NDVI.
+    
+    Retourne un dictionnaire dont les clés sont les étiquettes de classes
+    et les valeurs sont des dictionnaires contenant :
+        - "pixel_count" : nombre de pixels dans la classe
+        - "area_ha" : superficie en hectares
+        - "pourcentage_couverture" : pourcentage de couverture par rapport au nombre total de pixels
+    """
+    # trouve les classes presents et compte son occurence en terme de pixel 
+    unique_classes, pixel_counts = np.unique(ndvi_class_map, return_counts=True) # is array
+    
+    # un dict associant chaque classe  à son nombre de pixels.
+
+    dict_class_pixel_count = dict(zip(unique_classes, pixel_counts))
+    
+    total_pixels = np.sum(pixel_counts) # is integer
+    stats = {}
+    
+    for class_index, class_label in NDVI_CLASSES.items():
+        # nombre de pixels pour la classe actuelle
+        pixel_count = dict_class_pixel_count.get(class_index, 0)
+
+        total_surface_m2 = pixel_count * pixel_surface_m2
+        total_surface_ha = total_surface_m2 / 10000  # conversion en hectare
+
+        pourcentage_couverture = (pixel_count / total_pixels * 100) if total_pixels > 0 else 0
+
+        class_stats[class_label] = {
+            "pixel_count": pixel_count,
+            "area_ha": total_surface_ha,
+            "pourcentage_couverture": pourcentage_couverture
+        }
+
+    return class_stats
