2237aade6ee035bcda481953166bfa5f5251f4ca
Moussa Sow Dieme <lermitejiraya05@gmail.com>
2025-04-12 00:02:36 +0000

    test: Validation de la fonction calcul_class_stats

--- test_unitaire/test_ndvi.py
+++ test_unitaire/test_ndvi.py
@@ -4,7 +4,7 @@ import sys
 # si python ne parviens pas a acceder au dossier dashboard:l'ajouter au path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "dashboard")))
 
-from utils.data_loader import calcul_ndvi, classify_ndvi
+from utils.data_loader import calcul_ndvi, classify_ndvi, calcul_class_stats
 # -------------------------------------------------------------------------------------------
 # def test_calcul_ndvi(image_path):
     # pas necessaire
@@ -23,3 +23,30 @@ def test_classify_ndvi():
     #    }
     expected = [1, 1, 2, 3, 4] 
     assert class_map.tolist()[0] == expected
+# -------------------------------------------------------------------------------------------
+def test_calcul_class_stats(monkeypatch):
+    ndvi_class_map = np.array([
+        [1, 1, 2, 2],
+        [3, 3, 3, 4]
+    ])
+    
+   
+    monkeypatch.setattr("utils.data_loader.constantes.pixel_surface_m2", 100) 
+    fake_classes = {
+        1: "Eau ",
+        2: "Végétation clair",
+        3: "Végétation moyenne",
+        4: "Végétation dense"
+    }
+    monkeypatch.setattr("utils.data_loader.constantes.NDVI_CLASSES", fake_classes)
+    stats_to_test = calcul_class_stats(ndvi_class_map)
+    total_pixels = 8
+    pixel_surface_m2 = 100
+    for class_index, class_label in fake_classes.items():
+        compteur_pixels_true = np.sum(ndvi_class_map == class_index) # is int
+        area_ha_true = (compteur_pixels_true * pixel_surface_m2) / 10000
+        pourcentage_couverture_true = (compteur_pixels_true / total_pixels) * 100
+
+        assert stats_to_test[class_label]["pixel_count"] == compteur_pixels_true
+        assert stats_to_test[class_label]["area_ha"] == area_ha_true
+        assert stats_to_test[class_label]["pourcentage_couverture"] == pourcentage_couverture_true
